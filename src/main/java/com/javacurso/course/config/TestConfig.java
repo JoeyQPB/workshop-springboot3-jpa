456f8111c41142d1c5d44e9a87097a05ee4c2541
joey <joey_qpb@hotmail.com>
2023-11-15 21:24:00 +1100

    Many-to-many association with JoinTable

--- src/main/java/com/javacurso/course/config/TestConfig.java
+++ src/main/java/com/javacurso/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.javacurso.course.entities.Category;
 import com.javacurso.course.entities.Order;
+import com.javacurso.course.entities.OrderItem;
 import com.javacurso.course.entities.Product;
 import com.javacurso.course.entities.User;
 import com.javacurso.course.entities.eneums.OrderStatus;
 import com.javacurso.course.repositories.CategoryRepository;
+import com.javacurso.course.repositories.OrderItemRepository;
 import com.javacurso.course.repositories.OrderRepository;
 import com.javacurso.course.repositories.ProductRepository;
 import com.javacurso.course.repositories.UserRepository;
@@ -33,7 +35,10 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
-
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -68,5 +73,13 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1 ,p2 ,p3 ,p4 ,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 	}
 }
