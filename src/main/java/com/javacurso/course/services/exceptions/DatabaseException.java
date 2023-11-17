1fefebfac13ff547b1fa97f0159ee46fdf272cd6
joey <joey_qpb@hotmail.com>
2023-11-17 14:21:06 +1100

    add exceptions

---
+++ src/main/java/com/javacurso/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.javacurso.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
